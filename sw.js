5471b4fe822f413a08c1dab12ec2b1b5310dcdb3
centiger <centiger2@gmail.com>
2026-06-11 21:32:25 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='cen-jesus-matrix-v13';
+const CACHE='cen-jesus-matrix-v14-v14';
 const ASSETS=["./", "./index.html", "./manifest.json", "./assets/jesus-main-bg-final.png", "./hubs/index.html", "./hubs/data/hubs.json", "./hubs/assets/maps/incarnation-hub-map.png", "./hubs/assets/maps/ministry-hub-map.png", "./hubs/assets/maps/revelation-hub-map.png", "./hubs/assets/maps/cross-hub-map.png", "./hubs/assets/maps/resurrection-hub-map.png"];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).catch(()=>{}));self.skipWaiting();});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim();});
